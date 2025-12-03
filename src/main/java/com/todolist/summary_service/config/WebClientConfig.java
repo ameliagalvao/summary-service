351f3a419411729c5350672f2f3f176e250ff209
amelia <ameliamgalvao@gmail.com>
2025-12-03 20:59:44 -0300

    Paradigma reativo funcionando

--- src/main/java/com/todolist/summary_service/config/WebClientConfig.java
+++ src/main/java/com/todolist/summary_service/config/WebClientConfig.java
@@ -1,6 +1,7 @@
 package com.todolist.summary_service.config;
 
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -8,10 +9,13 @@ import org.springframework.web.reactive.function.client.WebClient;
 @Configuration
 public class WebClientConfig {
 
+    @Value("${task.service.url}")
+    private String taskServiceUrl;
+
     @Bean
     public WebClient taskWebClient(WebClient.Builder builder) {
         return builder
-                .baseUrl("http://localhost:8080")
+                .baseUrl(taskServiceUrl)
                 .build();
     }
 }
